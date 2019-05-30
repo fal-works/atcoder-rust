952cb6ced3b1f4a468bbf3f8ad4c371be69cf85f
FAL <falworks.contact@gmail.com>
2019-05-31 05:51:31 +0900

    Import FromStr

--- template_naive/src/main.rs
+++ template_naive/src/main.rs
@@ -1,12 +1,13 @@
 use std::io::*;
+use std::str::FromStr;
 
-fn parse_line<T: std::str::FromStr>() -> T {
+fn parse_line<T: FromStr>() -> T {
     let mut s = String::new();
     stdin().read_line(&mut s).ok();
     s.trim().parse().ok().unwrap()
 }
 
-fn parse_pair<T: std::str::FromStr, U: std::str::FromStr>() -> (T, U) {
+fn parse_pair<T: FromStr, U: FromStr>() -> (T, U) {
     let line = parse_line::<String>();
     let mut iter = line.split_whitespace();
     (
@@ -15,7 +16,7 @@ fn parse_pair<T: std::str::FromStr, U: std::str::FromStr>() -> (T, U) {
     )
 }
 
-fn parse_triple<T: std::str::FromStr, U: std::str::FromStr, V: std::str::FromStr>() -> (T, U, V) {
+fn parse_triple<T: FromStr, U: FromStr, V: FromStr>() -> (T, U, V) {
     let line = parse_line::<String>();
     let mut iter = line.split_whitespace();
     (
@@ -25,14 +26,14 @@ fn parse_triple<T: std::str::FromStr, U: std::str::FromStr, V: std::str::FromStr
     )
 }
 
-fn parse_line_vec<T: std::str::FromStr>() -> Vec<T> {
+fn parse_line_vec<T: FromStr>() -> Vec<T> {
     let line = parse_line::<String>();
     line.split_whitespace()
         .map(|s| s.parse().ok().unwrap())
         .collect()
 }
 
-fn parse_lines<T: std::str::FromStr>(n: usize) -> Vec<T> {
+fn parse_lines<T: FromStr>(n: usize) -> Vec<T> {
     let stdin = stdin();
     let mut stdin = stdin.lock();
 
