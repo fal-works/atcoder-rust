d12be04ecaa19208a6d1da6272f53da9bb53ffb5
FAL <falworks.contact@gmail.com>
2019-07-15 00:11:08 +0900

    Remove count_bits()

--- utils/util.rs
+++
@@ -1,8 +0,0 @@
-fn count_bits(bits: usize, len: usize) -> usize {
-    let mut bits = bits;
-    (0..len).fold(0, |acc, _| {
-        let next = if bits & 1 == 1 { acc + 1 } else { acc };
-        bits = bits >> 1;
-        next
-    })
-}
